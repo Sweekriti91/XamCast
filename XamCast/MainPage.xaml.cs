c8ead7e0af8b5dca85f66817ef572035932271d6
Sweekriti Satpathy <swsat@microsoft.com>
2021-05-12 12:37:19 -0700

    Adds XCT media element

--- XamCast/MainPage.xaml.cs
+++ XamCast/MainPage.xaml.cs
@@ -14,5 +14,25 @@ namespace XamCast
         {
             InitializeComponent();
         }
+
+        void OnMediaOpened(object sender, EventArgs e)
+        {
+            Console.WriteLine("Media opened.");
+        }
+
+        void OnMediaFailed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Media failed.");
+        }
+
+        void OnMediaEnded(object sender, EventArgs e)
+        {
+            Console.WriteLine("Media ended.");
+        }
+
+        void OnSeekCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Seek completed.");
+        }
     }
 }
