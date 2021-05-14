fb339d070154ba7eb2a78d533c9d2dccb1e73922
Sweekriti Satpathy <swsat@microsoft.com>
2021-05-13 19:11:13 -0700

    renderers added for iOS

---
+++ XamCast.iOS/Renderers/MyCastButtonRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Google.Cast;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamCast;
+using XamCast.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(MyCastButton), typeof(MyCastButtonRenderer))]
+namespace XamCast.iOS.Renderers
+{
+    public class MyCastButtonRenderer : ViewRenderer<MyCastButton, UICastButton>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<MyCastButton> e)
+        {
+            UICastButton castButton;
+
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    castButton = new UICastButton();
+                    SetNativeControl(castButton);
+                }
+            }
+        }
+    }
+}
---
+++ XamCast.iOS/Renderers/MyMiniPlayerRenderer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using CoreGraphics;
+using Google.Cast;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamCast;
+using XamCast.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(MyMiniPlayer), typeof(MyMiniPlayerRenderer))]
+namespace XamCast.iOS.Renderers
+{
+    public class MyMiniPlayerRenderer : ViewRenderer<MyMiniPlayer, UIView>
+    {
+        UIMiniMediaControlsViewController miniMediaControlsViewController;
+        UIView miniMediaControlsContainerView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MyMiniPlayer> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    var castContext = CastContext.SharedInstance;
+                    miniMediaControlsViewController = castContext.CreateMiniMediaControlsViewController();
+                    miniMediaControlsViewController.Delegate = new XamGoogleCastMiniControllerDelegate(this);
+                    miniMediaControlsContainerView = new UIView();
+                    miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 45);
+                    miniMediaControlsViewController.View.Frame = miniMediaControlsContainerView.Bounds;
+                    miniMediaControlsContainerView.AddSubview(miniMediaControlsViewController.View);
+                    UpdateControlBarsVisibility();
+                    SetNativeControl(miniMediaControlsContainerView);
+                }
+            }
+        }
+
+        public class XamGoogleCastMiniControllerDelegate : UIMiniMediaControlsViewControllerDelegate
+        {
+            MyMiniPlayerRenderer playerRenderer;
+
+            public XamGoogleCastMiniControllerDelegate(MyMiniPlayerRenderer pageR)
+            {
+                this.playerRenderer = pageR;
+            }
+
+            public override void ShouldAppear(UIMiniMediaControlsViewController miniMediaControlsViewController, bool shouldItAppear)
+            {
+                playerRenderer.UpdateControlBarsVisibility();
+            }
+        }
+
+        public void UpdateControlBarsVisibility()
+        {
+            Debug.WriteLine("miniMediaControlsViewController Active : " + miniMediaControlsViewController.Active);
+            if (miniMediaControlsViewController.Active)
+            {
+                Debug.WriteLine("TRUE YES MINI");
+                miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 55);
+                miniMediaControlsContainerView.SetNeedsDisplay();
+                miniMediaControlsContainerView.LayoutIfNeeded();
+            }
+            else
+            {
+                Debug.WriteLine("FALSE NO MINI");
+                miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 0);
+                miniMediaControlsContainerView.SetNeedsDisplay();
+                miniMediaControlsContainerView.LayoutIfNeeded();
+            }
+
+        }
+    }
+}
