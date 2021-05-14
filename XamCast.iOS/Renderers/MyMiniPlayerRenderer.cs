cc6a9fc4cfa47b743eb4158b50f69cb0f832b560
Sweekriti Satpathy <swsat@microsoft.com>
2021-05-13 19:42:07 -0700

    fixes permissions thing

--- XamCast.iOS/Renderers/MyMiniPlayerRenderer.cs
+++ XamCast.iOS/Renderers/MyMiniPlayerRenderer.cs
@@ -26,9 +26,47 @@ namespace XamCast.iOS.Renderers
                 {
                     var castContext = CastContext.SharedInstance;
                     miniMediaControlsViewController = castContext.CreateMiniMediaControlsViewController();
+                    //miniMediaControlsViewController.View.TintColor = Colors.Gold.ToUIColor();
+
+                    //var castStyle = UIStyle.SharedInstance;
+                    //castStyle.CastViews.MediaControl.MiniController.SliderProgressColor = Colors.Gold.ToUIColor();
+                    //castStyle.CastViews.MediaControl.MiniController.SliderSecondaryProgressColor = Colors.Gold.ToUIColor();
+                    //castStyle.CastViews.MediaControl.MiniController.SliderUnseekableProgressColor = Colors.Gold.ToUIColor();
+                    //castStyle.CastViews.MediaControl.MiniController.IconTintColor = Colors.Gold.ToUIColor();
+                    //castStyle.CastViews.MediaControl.MiniController.HeadingTextShadowColor = UIColor.Clear;
+
+                    //VisualTheme theme = AppInfo.RequestedTheme == AppTheme.Dark ? VisualTheme.Dark : VisualTheme.Light;
+
+                    //switch (theme)
+                    //{
+                    //    case VisualTheme.Light:
+                    //        {
+                    //            castStyle.CastViews.MediaControl.MiniController.BackgroundColor = Colors.LightBackground.ToUIColor();
+                    //            castStyle.CastViews.MediaControl.MiniController.HeadingTextColor = Colors.LightText.ToUIColor();
+
+                    //            break;
+                    //        }
+                    //    case VisualTheme.Dark:
+                    //        {
+                    //            castStyle.CastViews.MediaControl.MiniController.BackgroundColor = Colors.DarkBackground.ToUIColor();
+                    //            castStyle.CastViews.MediaControl.MiniController.HeadingTextColor = Colors.DarkText.ToUIColor();
+
+                    //            break;
+                    //        }
+                    //    default:
+                    //        {
+                    //            castStyle.CastViews.MediaControl.MiniController.BackgroundColor = Colors.LightBackground.ToUIColor();
+                    //            castStyle.CastViews.MediaControl.MiniController.HeadingTextColor = Colors.LightText.ToUIColor();
+
+                    //            break;
+                    //        }
+                    //}
+                    //castStyle.ApplyStyle();
+
                     miniMediaControlsViewController.Delegate = new XamGoogleCastMiniControllerDelegate(this);
                     miniMediaControlsContainerView = new UIView();
                     miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 45);
+
                     miniMediaControlsViewController.View.Frame = miniMediaControlsContainerView.Bounds;
                     miniMediaControlsContainerView.AddSubview(miniMediaControlsViewController.View);
                     UpdateControlBarsVisibility();
@@ -58,16 +96,12 @@ namespace XamCast.iOS.Renderers
             if (miniMediaControlsViewController.Active)
             {
                 Debug.WriteLine("TRUE YES MINI");
-                miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 55);
-                miniMediaControlsContainerView.SetNeedsDisplay();
-                miniMediaControlsContainerView.LayoutIfNeeded();
+                miniMediaControlsContainerView.Hidden = false;
             }
             else
             {
                 Debug.WriteLine("FALSE NO MINI");
-                miniMediaControlsContainerView.Frame = new CGRect(0, 0, 400, 0);
-                miniMediaControlsContainerView.SetNeedsDisplay();
-                miniMediaControlsContainerView.LayoutIfNeeded();
+                miniMediaControlsContainerView.Hidden = true;
             }
 
         }
